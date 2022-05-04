f7a87a68afc4ce1721ec9e296b6325fd46eb24c2
dooyeun <dooyeun330@naver.com>
2022-05-04 23:38:57 +0900

    register page and firebase

---
+++ app/src/main/java/com/example/greenwoodapp/login_page.java
@@ -0,0 +1,33 @@
+package com.example.greenwoodapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class login_page extends AppCompatActivity implements View.OnClickListener {
+
+    private TextView register;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_page2);
+
+        register = (TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.register:
+                startActivity(new Intent(this, register_page.class));
+                break ;
+
+
+        }
+    }
+}
